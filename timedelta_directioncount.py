5841d600d9c2e66223dc2d78964fcf36499ec924
Peter Sherman <shermanps@gmail.com>
2023-11-01 14:36:13 -0400

    Update timedelta_directioncount.py

--- timedelta_directioncount.py
+++ timedelta_directioncount.py
@@ -31,13 +31,13 @@ directions = ["NN", "NS", "NE", "NW", "SS", "SN", "SE", "SW", "EE", "EN", "ES", 
 # set range to number of 15 minute segments the script should run for. i.e. 40 = 10 hours
 # or set for two hour segments and call the script on schedule via cron
 # script can be called outside docker container
-for x in range(1,40):
+for x in range(1,8):
     #emptying result camera dictionary for the next iteration
     camera_dictionary = {}
     final_op=[]
     now = datetime.now()
     # set future to seconds=15*60 for 15 minutes
-    future = now + timedelta(seconds=10)
+    future = now + timedelta(seconds=30)
     print("x=",x," ",now, future)
     consumer = KafkaConsumer('direction', bootstrap_servers='localhost:9092')
     for message in consumer:
@@ -48,7 +48,7 @@ for x in range(1,40):
             # Parse JSON data
             data = json.loads(message_value)
             # Process the received JSON data
-            print(json.dumps(data, indent=4))
+            #print(json.dumps(data, indent=4))
             
             ######### Add logic here for turning movement counts
             for cam in cameras:
@@ -60,12 +60,12 @@ for x in range(1,40):
                 if data['id'] == cam:
                     # check all directions
                     for direc in directions:
-                        print(cam, "direction", direc, str(data['start_direction']+data['end_direction']))
-                        # if directions match
-                        if str(data['start_direction']+data['end_direction']) == direc:
+                        #print(cam, "direction", direc, str(data['start_direction']+data['end_direction']))
+                        # if directions match. str() to handle null values.
+                        if str(data['start_direction']) + str(data['end_direction']) == direc:
                             # access dictionary and increment direction value
-                            print(camera_dictionary[cam])
-                            print(camera_dictionary[cam][direc])
+                            #print(camera_dictionary[cam])
+                            #print(camera_dictionary[cam][direc])
                             camera_dictionary[cam][direc] += 1
 
             final_op.append(data)
@@ -74,20 +74,29 @@ for x in range(1,40):
             print("end time ", datetime.now())
             # close the consumer, otherwise the loop will not go to the next iteration
             consumer.close()
-    # put logic here to push to producer
-    
 
-    print(camera_dictionary)
 
-    #for cam in cameras:
-        #for mess in final_op:
-            #print(mess)
-            #if mess[1]
-
+    ##### put logic here to push to producer
+    #print(camera_dictionary)
+    #json_list = []
+    for key, value in camera_dictionary.items():
+        for direc2, direc2_value in value.items():
+            #greater_than_zero = {key: value for key, value in my_dict.items() if int(value) > 0}
+            if direc2_value > 0:
+                #val_to_append = key, direc2, direc2_value, datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                val_to_append = {"camera":key, "direction":direc2, "count":direc2_value, "time":datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+                #json_list.append(val_to_append)
+                print(json.dumps(val_to_append))
+                message_value = str(json.dumps(val_to_append)).encode('utf-8')
+                producer.send(topic, value=message_value)
+    #print(json_list)
     print("number of messages:", len(final_op))
-    message_value = str(final_op).encode('utf-8')
+    #encode for kafka
+    #message_value = str(final_op).encode('utf-8')
     # producer.send currently fails due to data in wrong format, needs JSON as output?
-    producer.send(topic, value=message_value)
+    #producer.send(topic, value=message_value)
+    ##### end producer logic
+
 # probably not necessary to close consumer here but just in case
 consumer.close()
 producer.close()
